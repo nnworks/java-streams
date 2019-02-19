0bc3e21f866d79abbca1eb731ca6d86e7656b54a
Theo Hoekstra <theo.hoekstra@noreply.com>
2019-02-19 14:00:44 +0100

    Updated Result and added unit tests

--- src/main/java/streams/Result.java
+++ src/main/java/streams/Result.java
@@ -4,97 +4,105 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
- * Class containing the result value and a possible throwable of a map operation
+ * Class containing the immutable result value and a possible throwable of a map operation
  * @param <V> the value type of the result
  */
 public class Result<V> {
-	V value;
-	Throwable throwable;
-	
-	public Result(V value, Throwable throwable) {
-	  this.value = value;
-	  this.throwable = throwable;
-	}
+    private final V value;
+    private final Throwable throwable;
 
-	public Result(V value) {
-	  this.value = value;
-	  this.throwable = null;
-	}
-	
-	public V getValue() {
-		return value;
-	}
-	
-	public Throwable getThrowable() {
-		return throwable;
-	}
-	
-	public boolean hasValue() {
-		return value != null;
-	}
-	
-	public boolean hasThrowable() {
-		return throwable != null;
-	}
-	
-	public String toString() {
-		return (hasValue() ? value.toString() : "") + ((hasThrowable()) ? " [throwable message: " + throwable.getMessage() + "]": "");
-	}
+    public Result(V value, Throwable throwable) {
+      this.value = value;
+      this.throwable = throwable;
+    }
 
-	/**
-	 * Methods for filtering and handling
-	 */
-	
-	public static enum Filter {
-		SUCCESS,
-		EXCEPTIONALS
-	}
-	
-	/**
-	 * Creates a filter to remove the success or the exceptional results.
-	 * @param filterExceptional the result to filter: when true, the exceptional results are filtered out, else the success results.
-	 * @return true or false depending filterExceptional given.
-	 */
-	public static <V> Predicate<Result<V>> createResultFilter(Filter filter) {
-		switch (filter) {
-		    case SUCCESS:
-				return (result) -> {
-					if (!result.hasThrowable()) {
-						return false;
-					} else {
-						return true;
-					}
-				};
-		    case EXCEPTIONALS:
-				return (result) -> {
-					if (result.hasThrowable()) {
-						return false;
-					} else {
-						return true;
-					}
-				};
-			default:
-				throw new IllegalArgumentException("Did not expect value " + filter + " for the filter");
-		}
-	}
-	
-	/**
-	 * creates a lambda function that calls the success function when the result is not exceptional, or the exceptional function when it is. 
-	 * @param success function called on success, with the result as parameter
-	 * @param exceptional function called in case the result has a throwable attached, with the result as parameter
-	 * @return the result of the success or exceptional call
-	 */
-	public static <V> Function<Result<V>, Result<V>> createHandler(Function<Result<V>, Result<V>> success, Function<Result<V>, Result<V>> exceptional) {
-		return (p) -> {
-			if (!p.hasThrowable()) {
-				return success.apply(p);
-			} else {
-				return exceptional.apply(p);
-			}
-		};
-	}
-	
-	
-	
-	
+    public Result(V value) {
+      this.value = value;
+      this.throwable = null;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+    public boolean hasValue() {
+        return value != null;
+    }
+
+    public boolean hasThrowable() {
+        return throwable != null;
+    }
+
+    public String toString() {
+        return (hasValue() ? value.toString() : "") + ((hasThrowable()) ? " [throwable message: " + throwable.getMessage() + "]": "");
+    }
+
+    /**
+     * Methods for filtering and handling
+     */
+
+    /**
+     * Enumeration with predefined filters.
+     * The SUCCESS filter will test true for results that have no throwable.
+     * The EXCEPTIONALS filter will test true for results that have a throwable.
+     */
+    public static enum Filter {
+        SUCCESS(false),
+        EXCEPTIONALS(true);
+
+        boolean filterExceptionals;
+
+        Filter(boolean filterExceptionals) {
+            this.filterExceptionals = filterExceptionals;
+        }
+
+        public boolean test(Result<?> result) {
+            if (filterExceptionals == result.hasThrowable()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Creates a stream filter (Predicate) to remove the success or the exceptional results.
+     * @param filter the result to let through: when Filter.SUCCESS, the exceptional results are removed, when Filter.EXCEPTIONALS the success results are removed from the result.
+     * @return true or false depending filter given.
+     */
+    public static <V> Predicate<Result<V>> createResultFilter(Filter filter) {
+        return (result) -> {
+            return filter.test(result);
+        };
+    }
+
+    /**
+     * creates a lambda function that calls the success function when the result is not exceptional, or the exceptional function when it is.
+     * @param successhandler function called on success, with the result as parameter
+     * @param exceptionalHandler function called in case the result has a throwable attached, with the result as parameter
+     * @return the result of the success or exceptional call
+     */
+    public static <V> Function<Result<V>, Result<V>> createHandler(Function<Result<V>, Result<V>> successhandler, Function<Result<V>, Result<V>> exceptionalHandler) {
+        return (p) -> {
+            if (!p.hasThrowable()) {
+                return successhandler.apply(p);
+            } else {
+                return exceptionalHandler.apply(p);
+            }
+        };
+    }
+
+    /**
+     * pulls out the value from the result.
+     * @param filter the result to let through: when Filter.SUCCESS, the exceptional results are removed, when Filter.EXCEPTIONALS the success results are removed from the result.
+     * @return true or false depending filter given.
+     */
+    public static <V> V extractValue(Result<V> result) {
+      return result.getValue();
+    }
+
 }
---
+++ src/test/java/streams/ResultTest.java
@@ -0,0 +1,132 @@
+package streams;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import streams.Result.Filter;
+
+import org.junit.jupiter.api.Assertions;
+
+class ResultTest {
+
+  List<String> list;
+  List<String> expectedFilteredResult;
+  List<String> expectedHandledResult;
+
+  String v = "value";
+  Throwable t = new Throwable();
+  Result<String> resultWithThrowable = new Result<String>(v, t);
+  Result<String> resultWithoutThrowable = new Result<String>(v);
+
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {
+  }
+
+  @AfterAll
+  static void tearDownAfterClass() throws Exception {
+  }
+
+  @BeforeEach
+  void setUp() throws Exception {
+    list = List.of("1", "2", "3", "4", "5", "6");
+    expectedFilteredResult = List.of("fwithNoExc(f(1))",
+                                     "fwithNoExc(f(2))",
+                                     "fwithNoExc(f(4))",
+                                     "fwithNoExc(f(5))");
+    expectedHandledResult = List.of("handledNormally(fwithNoExc(f(1)))",
+                                     "handledNormally(fwithNoExc(f(2)))",
+                                     "handledExceptionally(fwithExc(f(3)))",
+                                     "handledNormally(fwithNoExc(f(4)))",
+                                     "handledNormally(fwithNoExc(f(5)))",
+                                     "handledExceptionally(fwithExc(f(6)))");
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+  }
+
+  @Test
+  void testResult() {
+    Assertions.assertEquals(v, resultWithThrowable.getValue());
+    Assertions.assertEquals(t, resultWithThrowable.getThrowable());
+    Assertions.assertEquals(v, resultWithoutThrowable.getValue());
+    Assertions.assertEquals(null, resultWithoutThrowable.getThrowable());
+  }
+
+  @Test
+  void testCreateResultFilter() {
+
+    Predicate<Result<String>> keepExceptionals = Result.createResultFilter(Result.Filter.EXCEPTIONALS);
+
+    Assertions.assertTrue(keepExceptionals.test(resultWithThrowable));
+    Assertions.assertFalse(keepExceptionals.test(resultWithoutThrowable));
+
+    Predicate<Result<String>> removeExceptionals = Result.createResultFilter(Result.Filter.SUCCESS);
+
+    Assertions.assertFalse(removeExceptionals.test(resultWithThrowable));
+    Assertions.assertTrue(removeExceptionals.test(resultWithoutThrowable));
+  }
+
+  @Test
+  void testCreateResultFilterInStream() {
+     // Returning a list of Results containing the processed value and possibly an
+     // exception
+    List<String> result = list.stream()
+        .map(ResultTest::toResult)     										 // String in, Result out
+        .map(someExceptions())         										 // Result in, Result with sometimes an exception out
+        .filter(Result.createResultFilter(Filter.SUCCESS)) // Result in, Result out
+        .map(Result::extractValue)     										 // Result in, Value (String) out
+        .collect(Collectors.toList()); 										 // reduce to list with results
+
+    Assertions.assertArrayEquals(expectedFilteredResult.toArray(new String[expectedFilteredResult.size()]), result.toArray(new String[result.size()]));
+  }
+
+  @Test
+  void testResultHandlerInStream() {
+     // Returning a list of Results containing the processed value and possibly an
+     // exception
+    List<String> result = list.stream()
+        .map(ResultTest::toResult)     										 // String in, Result out
+        .map(someExceptions())         										 // Result in, Result with sometimes an exception out
+        .map(Result.createHandler(                         // Result in, Result out
+            (p)-> {
+              return new Result<String>("handledNormally(" + p.getValue() + ")");
+            },
+            (p) -> {
+              return new Result<String>("handledExceptionally(" + p.getValue() + ")");
+            }))
+        .map(Result::extractValue)     										 // Result in, Value (String) out
+        .collect(Collectors.toList()); 										 // reduce to list with results
+
+    Assertions.assertArrayEquals(expectedHandledResult.toArray(new String[expectedHandledResult.size()]), result.toArray(new String[result.size()]));
+  }
+
+
+  /**
+   * Helpers
+   */
+
+  private static Result<String> toResult(String in) {
+    return new Result<String>("f(" + in + ")");
+  }
+
+  private static Function<Result<String>, Result<String>> someExceptions() {
+    return (in) -> {
+      if (in.getValue().contains("(3)") || in.getValue().contains("(6)")) {
+        return new Result<String>("fwithExc(" + in.getValue() + ")",
+            new IllegalArgumentException("Exception: " + in.getValue()));
+      } else {
+        return new Result<String>("fwithNoExc(" + in.getValue() + ")");
+      }
+    };
+  }
+
+}
